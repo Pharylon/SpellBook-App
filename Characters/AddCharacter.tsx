649b5bc59ac54de888ff674f9960c79fc918fb68
GRUBCAVE-WIN\Pharylon <me@zachshuford.com>
2019-11-26 10:33:58 -0500

    Beta 1-1

--- Characters/AddCharacter.tsx
+++ Characters/AddCharacter.tsx
@@ -7,22 +7,23 @@ import Link from "../Link";
 import { BeyondCharacter } from "../Models/BeyondCharacter";
 import CharacterStore from "../Utility/CharacterStore";
 
-const myTestCharactersToLoad = [
-  "19233787",
-  "11598890",
-  "3312827",
-  "16871372",
-];
+// const myTestCharactersToLoad = [
+//   "19233787",
+//   "11598890",
+//   "3312827",
+//   "16871372",
+// ];
 
 export default function AddCharacterScreen(props: NavigationStackScreenProps) {
-  const [inputValue, setInputValue] = useState(myTestCharactersToLoad[0]);
+  // const [inputValue, setInputValue] = useState(myTestCharactersToLoad[0]);
+  const [inputValue, setInputValue] = useState("");
   const addCharacter = (newCharacter: BeyondCharacter) => {
     CharacterStore.saveCharacter(newCharacter);
     props.navigation.navigate("Home", {newId: newCharacter.id});
   };
   const getCharacter = async () => {
-    const index = myTestCharactersToLoad.indexOf(inputValue) + 1;
-    setInputValue(myTestCharactersToLoad[index]);
+    // const index = myTestCharactersToLoad.indexOf(inputValue) + 1;
+    // setInputValue(myTestCharactersToLoad[index]);
     let characterId = inputValue;
     if (characterId.toLowerCase().startsWith("http")){
       characterId = characterId.substring(characterId.lastIndexOf("/"));
@@ -44,7 +45,8 @@ export default function AddCharacterScreen(props: NavigationStackScreenProps) {
         <Link text="your character page" uri="https://www.dndbeyond.com/my-characters" /> on D&amp;D Beyond,
               click "View" on the character whose spells you wish to browse.
               Look at the URL in your browser address bar. It should end in a number.
-              Enter that number below. Note that the character must be public.
+              Enter that number below. You can also copy/paste the entire URL. 
+              Note that the character must be public.
         </Text>
       </View>
       <View style={Styles.inputWrapper}>
